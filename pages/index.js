ba01af1821249dab522ad3477fa1126789f12fbc
Emiliano Castellano <ecastellanorobaina@gmail.com>
2021-12-28 23:36:40 -0300

    Updated

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home() {
       <Link mt='5' textDecoration='underline' href='https://goo.gl/maps/xm8dgn2kfkE7vLAP6' isExternal>Mapa</Link>
     </Box>
     <Box bg='#c9c293' padding='10' display='flex' justifyContent='center' flexDirection='column' alignItems='center'>
-      <Text fontSize='25' mb='5'>Cuenta regresiva</Text>
+      <Text fontSize='25' mb='5'>Solo faltan</Text>
       {partyTime ? (
         <Text fontSize='30'>Empezó 🎉</Text>
       ) : <Box display='flex'>
